904adee4b79167dd0b5d1d3ad21030d0dc3a6064
Daniel Klinkert <6820828+klinkert0728@users.noreply.github.com>
2024-11-02 12:40:02 +0100

    run benchmark in sut

--- setup/scripts/configure_tsbs.sh
+++ setup/scripts/configure_tsbs.sh
@@ -31,13 +31,34 @@ echo "PATH=$PATH" | sudo tee -a /etc/environment
 echo "TSBS_PATH=$TSBS_PATH" | sudo tee -a /etc/environment
 
 ### Generate Data
-tsbs_generate_data --use-case=devops --seed=123 --scale=800  --timestamp-start="2024-10-26T00:00:00Z" --timestamp-end="2024-10-27T00:00:00Z" --log-interval="60s" --format="victoriametrics" > ~/data.txt
+tsbs_generate_data --use-case=devops --seed=123 --scale=800  --timestamp-start="2024-10-26T00:00:00Z" --timestamp-end="2024-10-31T00:00:00Z" --log-interval="60s" --format="victoriametrics" > ~/data.txt
 
 ### Generate Queries
 # Keep in mind the timestamp start and end should be the same as the data generated.
-tsbs_generate_queries --use-case=devops --seed=123 --scale=800 --timestamp-start="2024-10-26T00:00:00Z" --timestamp-end="2024-10-27T00:00:00Z" --queries=1440 --query-type=double-groupby-all --format=victoriametrics > ~/queries.txt
+tsbs_generate_queries --use-case=devops --seed=123 --scale=800 --timestamp-start="2024-10-26T00:00:00Z" --timestamp-end="2024-10-31T00:00:00Z" --queries=1440 --query-type=double-groupby-all --format=victoriametrics > ~/queries.txt
 
 mkdir -p $HOME/results
 
 ### Load data into sut
-tsbs_load_victoriametrics --urls="http://$SUT_IP_AND_PORT_LATEST/write" --workers=10 --batch-size=100 --file="$HOME/data.txt" --latencies-file=$HOME/results/latenciesInserts.csv 2>&1 | tee $HOME/results/logInserts.log & tsbs_load_victoriametrics --urls="http://$SUT_IP_AND_PORT_OTHER/write" --workers=10 --batch-size=100 --file="$HOME/data.txt" --latencies-file=$HOME/results/latenciesInserts_other.csv 2>&1 | tee $HOME/results/logInserts_other.log
+loadData() {
+    IP=$1
+    VERSION=$2
+    tsbs_load_victoriametrics --urls="http://$IP/write" --workers=10 --batch-size=100 --file="$HOME/data.txt" --latencies-file=$HOME/results/latenciesInserts_${VERSION}.csv 2>&1 | tee $HOME/results/logInserts_${VERSION}.log 
+}
+
+loadData $SUT_IP_AND_PORT_LATEST "latest" & 
+loadData $SUT_IP_AND_PORT_OTHER "other"
+
+runBenchmark() {
+    IP=$1
+    VERSION=$2
+    tsbs_run_queries_victoriametrics --file=$HOME/queries.txt \
+		--urls="http://$IP" \
+		--latencies-file=results/latenciesQueries${VERSION}.csv  \
+		--print-interval="500" \
+		--workers=10 | tee results/logQueries${VERSION}.log
+
+}
+
+runBenchmark $SUT_IP_AND_PORT_LATEST "latest" & 
+runBenchmark $SUT_IP_AND_PORT_OTHER "other" &
