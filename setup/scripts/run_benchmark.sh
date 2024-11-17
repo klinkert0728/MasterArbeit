9cc78061f60dd5b713f1a6385793f0ee9d7c18fe
Daniel Klinkert <6820828+klinkert0728@users.noreply.github.com>
2024-11-17 13:14:25 +0100

    improve collecting data

--- setup/scripts/run_benchmark.sh
+++ setup/scripts/run_benchmark.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
+echo "Running microbenchmark" > /home/benchUser/results/proof.txt
+
 ansible-playbook -i hosts.yml ./configure_micro_benchmark_client.yml --ssh-common-args='-o StrictHostKeyChecking=no' &
 MICRO_PID_1=$!
 ansible-playbook -i hosts.yml ./configure_application_benchmark_client.yml --ssh-common-args='-o StrictHostKeyChecking=no' &
 APPLICATION_PID_2=$!
 wait $MICRO_PID_1 $APPLICATION_PID_2
+
+echo "Finished microbenchmark" >> /home/benchUser/results/proof.txt
